d4462c7fd42117ee6c04e5ab6bb813500127c27c
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-04-05 21:09:43 +0300

    Finish with one way bindings

--- angular_theory/src/app/app.component.ts
+++ angular_theory/src/app/app.component.ts
@@ -5,6 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+//вся логика описывается в классах
 export class AppComponent {
-  title = 'angular_theory';
+  title: string = 'Dynamic Title';
+
+  number: number = 34;
+
+  arr: number[] = [1, 2, 3,];
+
+  obj = {
+    a: 1,
+    b: {
+      c: 4,
+    },
+  };
+
+  img: string = 'https://encrypted-tbn0.gstatic.com/images?q=\
+  tbn:ANd9GcThGyyY4OZ3bk3rFDaYtAbHR8htxrLHnjw2nxRL_80Xs7F0KG8-4dgIxP-wtQKFhdyXyvQ&usqp=CAU';
+
+  inputText: string = '';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over');
+
+      this.img = 'https://encrypted-tbn0.gstatic.com/\
+      images?q=tbn:ANd9GcR3-XHgiNJWPedplE0FHDZFYGCygQkYNTPgAvoox4-BmuORc40M7xg0-5ZDkh-HMKdc3eY&usqp=CAU'
+    }, 4000);
+  };
+
+  //стандартный объект ивента нужно передавать в вызов
+  //обработчика с шаблона с помощю специального синтаксиса
+  onInput(event: Event) {
+    this.inputText = (<HTMLInputElement>event.target).value;
+  };
+
+  onBlur(str: string) {
+    this.inputText = str;
+  };
 }
